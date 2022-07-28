0ab0bb2e1fbeb719e43f9f49c06fe9039359cd8c
anik <anikbhadra01@gmail.com>
2022-07-28 21:48:51 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,54 @@ function App() {
   const [age, setAge] = useState(0)
   const [height, setHeight] = useState(0)
   const [superpower, setSuperPower] = useState("")
-
-
-  const changeName = () => {
-    setName("Bhadra")
-  }
+  const [displayCharacter, setDisplayCharacter] = useState(false)
 
   return (
-    <div >
-      <h1>Build A Hero</h1>
-      <label>Name:</label>
-      <input type="text" style={{ margin: 10 }}
-        onChange={(event) => {
-          setName(event.target.value)
-        }} />
+    <div>
+      <div >
+        <h1>Build A Hero</h1>
+        <label>Name:</label>
+        <input type="text" style={{ margin: 10 }}
+          onChange={(event) => {
+            setName(event.target.value)
+          }} />
 
-      <label>Age:</label>
-      <input type="number" style={{ margin: 10 }}
-        onChange={(event) => {
-          setAge(event.target.value)
-        }}
-      />
+        <label>Age:</label>
+        <input type="number" style={{ margin: 10 }}
+          onChange={(event) => {
+            setAge(event.target.value)
+          }}
+        />
 
-      <label>Height:</label>
-      <input type="text" style={{ margin: 10 }}
-        onChange={(event) => {
-          setHeight(event.target.value)
-        }}
-      />
+        <label>Height:</label>
+        <input type="text" style={{ margin: 10 }}
+          onChange={(event) => {
+            setHeight(event.target.value)
+          }}
+        />
 
-      <label>SuperPower:</label>
-      <input type="text" style={{ margin: 10 }}
-        onChange={(event) => {
-          setSuperPower(event.target.value)
-        }}
-      />
+        <label>SuperPower:</label>
+        <input type="text" style={{ margin: 10 }}
+          onChange={(event) => {
+            setSuperPower(event.target.value)
+          }}
+        />
 
-      <button onClick={changeName}>change name</button>
-      {name}
-      {age}
-      {height}
-      {superpower}
+      </div>
+      <button onClick={() => setDisplayCharacter(true)} >Display Character</button>
+      <div>
+        <h1>Hero Info</h1>
+        {displayCharacter && (
+          <ul>
+            <li>Name: {name}</li>
+            <li>Age {age}</li>
+            <li>Height: {height}</li>
+            <li>SuperPower: {superpower}</li>
+          </ul>
+        )}
+      </div>
     </div>
+
   )
 }
 

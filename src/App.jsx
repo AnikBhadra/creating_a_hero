3f8f8d61b8ffd11357da1e898eb4e98ac393da14
anik <anikbhadra01@gmail.com>
2022-07-28 21:29:02 +0530

    changing states

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 
 function App() {
 
-  const [name, setName] = useState("anik")
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [superpower, setSuperPower] = useState("")
 
 
   const changeName = () => {
@@ -14,19 +17,37 @@ function App() {
     <div >
       <h1>Build A Hero</h1>
       <label>Name:</label>
-      <input type="text" style={{ margin: 10 }} />
+      <input type="text" style={{ margin: 10 }}
+        onChange={(event) => {
+          setName(event.target.value)
+        }} />
 
       <label>Age:</label>
-      <input type="number" style={{ margin: 10 }} />
+      <input type="number" style={{ margin: 10 }}
+        onChange={(event) => {
+          setAge(event.target.value)
+        }}
+      />
 
       <label>Height:</label>
-      <input type="text" style={{ margin: 10 }} />
+      <input type="text" style={{ margin: 10 }}
+        onChange={(event) => {
+          setHeight(event.target.value)
+        }}
+      />
 
       <label>SuperPower:</label>
-      <input type="text" style={{ margin: 10 }} />
+      <input type="text" style={{ margin: 10 }}
+        onChange={(event) => {
+          setSuperPower(event.target.value)
+        }}
+      />
 
       <button onClick={changeName}>change name</button>
       {name}
+      {age}
+      {height}
+      {superpower}
     </div>
   )
 }
